f2ed6505968c1d649696bb29546ed2e734e3032d
JoabFelippx <joabfelippe30@gmail.com>
2021-07-12 22:16:48 -0300

    Listar os arquivos e pegar a extensão

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import time
 
+
 import os
 from pathlib import Path
+import filetype
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+
+# definir o caminho da pasta download da maquina
+DOWNLOAD_FOLDER = str(Path.home() / "Downloads")
+
+
+class Program(FileSystemEventHandler):
+
+    # função que modifica arquivos ou pastas
+    def on_modified(self, event):
+
+        # listar ariquvos da pasta DOWNLOAD
+        for filename in os.listdir(DOWNLOAD_FOLDER):
+
+            file_path = os.path.join(DOWNLOAD_FOLDER, filename)
+
+            try:
+                kind = filetype.guess(file_path)
+                file_extension = kind.extension
+                print(file_extension, kind.mime, file_path)
+            except:
+                print("no extension")
